b8e3b38273472cc0ea258217b9bbf67ae48b70d1
Gianmarco Gutierrez <g.gutierrez@uni.pe>
2021-07-23 14:30:10 -0500

    add assets

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDJwJv0ntahtPhNdtyqg30D4nsiVupp31A",
+  authDomain: "cotizacion-cbcaf.firebaseapp.com",
+  projectId: "cotizacion-cbcaf",
+  storageBucket: "cotizacion-cbcaf.appspot.com",
+  messagingSenderId: "410974769111",
+  appId: "1:410974769111:web:231a6f2ad543928b200592",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
